ab45b79689143150abe948836de38dfacdd95e0d
lzqiang1020 <liuzhiqiang1020@163.com>
2019-01-30 15:51:23 +0800

    ForeignKey增加on_delete=models.CASCADE

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class BlogArticles(models.Model):
     title = models.CharField(max_length=300)
-    author = models.ForeignKey(User, related_name="blog_posts")
+    author = models.ForeignKey(User, related_name="blog_posts", on_delete=models.CASCADE)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
 
